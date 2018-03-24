db982400fc3eca7afa2d8779367143da87e353b7
Reb0rn <exodus@smtp.ru>
2018-03-24 18:16:37 +0300

    Update copy from 7.94.bat

--- copy from 7.94.bat
+++ copy from 7.94.bat
@@ -1,13 +1,13 @@
-robocopy \\10.200.7.94\Archive\Today\ d:\PKI\Crl_loader\Certificates\ *.cer /LOG+:d:\PKI\Crl_loader\log1.txt
+robocopy \\10.200.1.1\Archive\Today\ d:\PKI\Crl_loader\Certificates\ *.cer /LOG+:d:\PKI\Crl_loader\log1.txt
 forfiles -p "D:\PKI\Crl_loader\Certificates" -s -m *.* -d -1 -c "cmd /c del @path"
-robocopy \\10.200.7.94\Archive\Fastload\ d:\PKI\Crl_loader\Certificates\ *.cer
-robocopy \\10.200.7.94\Archive\Fastload\ d:\PKI\Crl_loader\Certificates\ *.p7b
+robocopy \\10.200.1.1\Archive\Fastload\ d:\PKI\Crl_loader\Certificates\ *.cer
+robocopy \\10.200.1.1\Archive\Fastload\ d:\PKI\Crl_loader\Certificates\ *.p7b
 
 @echo off
 setlocal
 for /f  "usebackq delims=;" %%A in (`dir /b *.log`) do If %%~zA GTR 32134287 del "%%A"
 
-rem все в байтах
+
 
 @echo off
 setlocal
